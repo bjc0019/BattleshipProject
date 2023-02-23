0ee77ebccb747b7d5ab461671482f560fab4b6ea
bcalv <bcalv@Lenovo-Laptop.attlocal.net>
2023-02-23 14:11:39 -0600

    Added javadocs, ability to generate random boards, and setters/getters for board class. Added random location method and default constructors to ship superclass.

---
+++ src/battleship/Battleship.java
@@ -0,0 +1,23 @@
+package battleship;
+import battleship.Ships.*;
+import myExceptions.*;
+
+public class Battleship {
+    public static void main(String[] args) {
+        Board fboard = new Board();
+        Carrier carrier = new Carrier(2,1,true);
+        BattleShip battleship = new BattleShip(1,1,true);
+        Submarine submarine = new Submarine(4,4,true);
+        Cruiser cruiser = new Cruiser(4,3, false);
+        Destroyer destroyer = new Destroyer(7,6,true);
+        
+        //System.out.println(fboard.placeShip(carrier));
+        //System.out.println(fboard.placeShip(battleship));
+        //System.out.println(fboard.placeShip(cruiser));
+        //System.out.println(fboard.placeShip(submarine));
+        //System.out.println(fboard.placeShip(destroyer));
+        
+        fboard.randomizeBoard();
+        fboard.printBoard();
+    } 
+}
