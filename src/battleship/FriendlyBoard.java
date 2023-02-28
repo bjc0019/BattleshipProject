b6814c2acfc8ed62bf3752b7b45ca620d86aca9e
bcalv <bcalv@Lenovo-Laptop.attlocal.net>
2023-02-28 13:32:08 -0600

    Minor Changes to FriendlyBoard and OpponentBoard, namely making setTile private instead of public.

--- src/battleship/FriendlyBoard.java
+++ src/battleship/FriendlyBoard.java
@@ -1,29 +1,158 @@
 package battleship;
 import battleship.Ships.*;
-import java.util.Arrays;
-import java.util.Collections;
+import myExceptions.*;
+
 
 public class FriendlyBoard {
+    // *********************************************************************************************
     // Variable Declaration
-    private char[][] board;     // 8x8 array which holds ship locations in char representation
-    private Carrier carrier;        // Classes which hold the 5 ship locations
+    // *********************************************************************************************
+    
+    /** Defines the size of the battleship board as n by n grid. The rules state this should be 10. */
+    public static final int BOARD_SIZE = 10;
+    
+    /** BOARD_SIZE x BOARD_SIZE array which holds ship locations in char representation. */
+    private char[][] board;
+    
+    /** Current location of Carrier on this board, can be null if ship hasn't been placed yet. */ 
+    private Carrier carrier;  
+    
+    /** Current location of BattleShip on this board, can be null if ship hasn't been placed yet. */
     private BattleShip battleship;
+    
+    /** Current location of Submarine on this board, can be null if ship hasn't been placed yet. */
     private Submarine submarine;
+    
+    /** Current location of Cruiser on this board, can be null if ship hasn't been placed yet. */
     private Cruiser cruiser;
+    
+    /** Current location of Destroyer on this board, can be null if ship hasn't been placed yet. */
     private Destroyer destroyer;
     
-    /* Board constructor for Board class to be used as friendly board, containing 
-       'E' for Empty or 'A', 'B', 'S', 'C', or 'D' to represent the ship types */
+    // *********************************************************************************************
+    // Constructors
+    // *********************************************************************************************
     
+    /** Default board constructor which initializes an empty board. */
     public FriendlyBoard()
     {
-        board = new char[8][8];
+        board = new char[BOARD_SIZE][BOARD_SIZE];
         resetBoard();
     }
     
+    // *********************************************************************************************
     // Methods
+    // *********************************************************************************************
     
-    // Places ship on board and returns true if placement is valid, false otherwise
+    /** 
+     * Returns the char representation of the board at coordinates (x,y)
+     @return char 'E' (empty), 'A'(Aircraft Carrier), 'B'(Battleship), 'S'(Submarine), 'C'(Cruiser), or 'D'(Destroyer) 
+     @param _x x coordinate on the board, starting at 0 index
+     @param _y y coordinate on the board, starting at 0 index
+     @throws IndexOutOfBounds exception if either x or y is not within the board
+     */
+    public char getTile(int _x, int _y) throws IndexOutOfBounds
+    {
+        if (_x < BOARD_SIZE && _y < BOARD_SIZE && _x >= 0 && _y >= 0)
+            return board[(_x)][_y];
+        else
+            throw new myExceptions.IndexOutOfBounds("Invalid x,y pair (" + _x + ", " + _y + ')');
+    }
+    
+    
+    /** 
+     * Returns current instance of ship _shipType, which can be a Ship instance
+     * or null, depending on current board state. 
+     * @param _shipType One of 'A', 'B', 'C', 'D', or 'S'.
+     * @return Either null or a Ship subclass
+     */
+    public Ship getShip(char _shipType)
+    {
+        return switch(_shipType) {
+            case 'A' -> carrier;
+            case 'B' -> battleship;
+            case 'C' -> cruiser;
+            case 'D' -> destroyer;
+            case 'S' -> submarine;
+            default -> throw new IllegalArgumentException("Input should be char A,B,C,D,or S.");
+        };
+    }
+    
+    
+    /** Clears and resets the board with randomized ship placement. */
+    public void randomizeBoard()
+    {
+        // First, clear the board
+        resetBoard();
+        
+        // Declare holding variables for new ships
+        Carrier newCarrier = new Carrier();
+        BattleShip newBattleship = new BattleShip();
+        Cruiser newCruiser = new Cruiser();
+        Submarine newSubmarine = new Submarine();
+        Destroyer newDestroyer = new Destroyer();
+        
+        
+        // Randomly each ship on the board until it fits
+        while (placeShip(newCarrier) == false)
+            newCarrier.randomizeLocation();   
+        
+        while (placeShip(newBattleship) == false)
+            newBattleship.randomizeLocation();
+        
+        while (placeShip(newCruiser) == false)
+            newCruiser.randomizeLocation();
+        
+        while (placeShip(newSubmarine) == false)
+            newSubmarine.randomizeLocation();
+        
+        while (placeShip(newDestroyer) == false)
+            newDestroyer.randomizeLocation();
+    }
+    
+    
+    /** 
+     * Sets the board at coordinates (x,y) to char _in
+     @param _x x coordinate on the board, starting at 0 index
+     @param _y y coordinate on the board, starting at 0 index
+     @param _input char that gets put in the board
+     @throws IndexOutOfBounds exception if either x or y is not within the board
+     */
+    private void setTile(int _x, int _y, char _input) throws IndexOutOfBounds
+    {
+        if (_x < BOARD_SIZE && _y < BOARD_SIZE && _x >= 0 && _y >= 0)
+            board[_x][_y] = _input;
+        else
+            throw new myExceptions.IndexOutOfBounds("Invalid (x,y) pair (" + _x + ", " + _y + ')');
+    }
+    /** Prints the current board to the console, used for debugging. */
+    public void printBoard()
+    {
+        for(int i = 0; i < BOARD_SIZE; i++) {
+            for(int j = 0; j < BOARD_SIZE; j++) 
+                try {
+                    if (this.getTile(j, BOARD_SIZE - 1 - i) == 'E')
+                        System.out.print(" ");
+                    else
+                        System.out.print(this.getTile(j, BOARD_SIZE - 1 - i));
+                    System.out.print(", ");
+                }
+                catch(IndexOutOfBounds ex) {
+                    System.out.println("Error printing board to the terminal: " + ex.getMessage());
+                    System.exit(1);
+                }
+                        
+            System.out.println("");
+        }
+    }
+    
+
+    /** 
+     * Places ship on the board and returns true if successful, or false if 
+     * the ship placement is invalid. 
+     * @param _ship Ship subclass to be placed 
+     * @return True or false, if the placement is valid and ship was placed
+     */
     public boolean placeShip(Ship _ship)
     {
         // Check to see if ship placement is valid
@@ -65,11 +194,17 @@ public class FriendlyBoard {
             destroyer = (Destroyer)_ship;
         }      
         
-        
-        
+        // Once ship is placed validly, return true
         return true;
     }
     
+    
+    /**
+     * Returns true or false if the ship parameter can be placed 
+     * on the board without intersecting other ships or running off the board
+     * @param _ship Ship subclass to be placed
+     * @return True or false, if the ship placement is valid
+     */
     private boolean isShipPlacementValid(Ship _ship)
     {
         // First, check bounds for ship placement
@@ -78,7 +213,7 @@ public class FriendlyBoard {
         
         // Check bounds and overlapping tiles for horizontal placement
         if(_ship.isVertical) {
-            if(_ship.y + _ship.length - 1 > 7)
+            if(_ship.y + _ship.length > BOARD_SIZE)
                 return false;
             
             for(int i = 0; i < _ship.length; i++)
@@ -86,7 +221,7 @@ public class FriendlyBoard {
                     return false;
         }
         else {
-            if(_ship.x + _ship.length - 1 > 7)
+            if(_ship.x + _ship.length > BOARD_SIZE)
                 return false;
             
             for(int i = 0; i < _ship.length; i++)
@@ -98,12 +233,16 @@ public class FriendlyBoard {
         return true;
     }
     
-    // Clears the board and sets all values to 'E'
+    
+    /** 
+     * Resets the internal board, setting all squares to 'E' for empty, and
+     * sets all internal ship placement variables to null.
+     */
     private void resetBoard()
     {
         // Set all values in the char array to 'E' (Empty)
-        for(int i = 0; i < 8; i++) {
-            for(int j = 0; j < 8; j++) {
+        for(int i = 0; i < BOARD_SIZE; i++) {
+            for(int j = 0; j < BOARD_SIZE; j++) {
                 board[i][j] = 'E';
             }
         }
@@ -114,16 +253,6 @@ public class FriendlyBoard {
         cruiser = null;
         submarine = null;
         destroyer = null;
+        
     }
-    
-    // Prints the board to the console
-    public void printBoard()
-    {
-        for(int i = 7; i >= 0; i--) {
-            for(int j = 0; j <= 7; j++) 
-                System.out.print(board[j][i] + ", " );
-            System.out.println("");
-        }
-    }
-    
 }
